49fe604d90a0b7144d026366109f039360a02246
vitor <zorinos.masterzorin@outlook.com>
2024-06-12 23:21:46 -0300

    Update: 12/06/2024

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace App;
+
+public class Program
+{
+
+    private int idade;
+    private string name;
+    private decimal altura;
+    private new DateTime data;
+    
+    public static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.resultado();
+        Console.WriteLine($"{program.name}");
+        Console.WriteLine($"{program.idade}");
+        Console.WriteLine($"{program.altura}");
+        Console.WriteLine($"{program.data}");
+    }
+
+    public void resultado()
+    {
+        this.idade = 23;
+        this.altura = 1.70m;
+        this.name = "Vitor";
+        this.data = new DateTime(2001,04,03);
+    }
+}
+
